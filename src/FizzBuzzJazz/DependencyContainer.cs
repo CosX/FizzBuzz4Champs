3134474fd296871ef09c9ed05006d4507352749c
Karl Solgård <karl.f91@gmail.com>
2019-03-29 16:42:31 +0100

    DI in static class

---
+++ src/FizzBuzzJazz/DependencyContainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using FizzBuzzJazz.Implementation;
+using FizzBuzzJazz.Implementation.Rules;
+using FizzBuzzJazz.Models.Enums;
+using FizzBuzzJazz.Models.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FizzBuzzJazz.Console
+{
+    public static class DependencyContainer
+    {
+        public static ServiceProvider BuildServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddTransient<FizzRule>()
+                .AddTransient<BuzzRule>()
+                .AddTransient<JazzRule>()
+                .AddTransient<FuzzRule>()
+                .AddTransient(factory =>
+                {
+                    return (Func<RuleKey, IRule>)(key =>
+                    {
+                        switch (key)
+                        {
+                            case RuleKey.Fizz:
+                                return factory.GetService<FizzRule>();
+                            case RuleKey.Buzz:
+                                return factory.GetService<BuzzRule>();
+                            case RuleKey.Jazz:
+                                return factory.GetService<JazzRule>();
+                            case RuleKey.Fuzz:
+                                return factory.GetService<FuzzRule>();
+                            default:
+                                throw new KeyNotFoundException();
+                        }
+                    });
+                })
+                .AddTransient<IGameService, GameService>()
+                .AddTransient<DirectionGenerator>()
+                .BuildServiceProvider();
+        }
+    }
+}
--- src/FizzBuzzJazz/Program.cs
+++ src/FizzBuzzJazz/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using FizzBuzzJazz.Implementation;
-using FizzBuzzJazz.Implementation.Rules;
-using FizzBuzzJazz.Models.Enums;
+﻿using FizzBuzzJazz.Models.Enums;
 using FizzBuzzJazz.Models.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,33 +8,7 @@ namespace FizzBuzzJazz.Console
     {
         static void Main(string[] args)
         {
-            ServiceProvider serviceProvider = new ServiceCollection()
-                .AddTransient<FizzRule>()
-                .AddTransient<BuzzRule>()
-                .AddTransient<JazzRule>()
-                .AddTransient<FuzzRule>()
-                .AddTransient(factory =>
-                {
-                    return (Func<RuleKey, IRule>)(key =>
-                    {
-                        switch (key)
-                        {
-                            case RuleKey.Fizz:
-                                return factory.GetService<FizzRule>();
-                            case RuleKey.Buzz:
-                                return factory.GetService<BuzzRule>();
-                            case RuleKey.Jazz:
-                                return factory.GetService<JazzRule>();
-                            case RuleKey.Fuzz:
-                                return factory.GetService<FuzzRule>();
-                            default:
-                                throw new KeyNotFoundException();
-                        }
-                    });
-                })
-                .AddTransient<IGameService, GameService>()
-                .AddTransient<DirectionGenerator>()
-                .BuildServiceProvider();
+            ServiceProvider serviceProvider = DependencyContainer.BuildServiceProvider();
 
             IGameService engine = serviceProvider.GetService<IGameService>();
 
