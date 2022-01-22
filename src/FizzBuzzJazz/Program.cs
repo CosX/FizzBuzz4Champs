28fcd9d9daba163f3765bbef27ed4dff463fa1b9
Karl Solgård <karl.f91@gmail.com>
2022-01-22 12:55:14 +0100

    Reflection

--- src/FizzBuzzJazz/Program.cs
+++ src/FizzBuzzJazz/Program.cs
@@ -1,17 +1,24 @@
-﻿using FizzBuzzJazz;
+﻿using System.Linq;
+using System.Reflection;
+using FizzBuzzJazz;
 using FizzBuzzJazz.Rules;
 using Microsoft.Extensions.DependencyInjection;
 
-var serviceProvider = new ServiceCollection()
-    .AddTransient<IRule, FizzRule>()
-    .AddTransient<IRule, BuzzRule>()
-    .AddTransient<IRule, JazzRule>()
-    .AddTransient<IRule, FuzzRule>()
-    .AddTransient<Game>()
-    .BuildServiceProvider();;
+var rules = Assembly.GetCallingAssembly().GetTypes()
+    .Where(type => typeof(IRule).IsAssignableFrom(type) && !type.IsInterface);
+
+var serviceCollection = new ServiceCollection()
+    .AddTransient<Game>();
+
+foreach (var rule in rules)
+    serviceCollection.AddTransient(typeof(IRule), rule);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
 
 var engine = serviceProvider.GetService<Game>();
 foreach (var result in engine!.GetResults(1, 100))
     System.Console.WriteLine(result);
 
 System.Console.ReadKey();
+
+serviceProvider.Dispose();
